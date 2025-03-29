7d800dfabcb5daea66bb44f62cc55782a5740a62
adit <aditya@winanda.net>
2025-03-29 08:09:01 +0700

    add horoscope calculation

--- src/profile.service.ts
+++ src/profile.service.ts
@@ -14,6 +14,46 @@ export enum MeasurementUnit {
   US = "us" // pound
 }
 
+interface HoroscopeRef {
+  startDate: number;
+  startMonth: number;
+  endDate: number;
+  endMonth: number;
+  name: string;
+}
+
+const horoscopeReferences: HoroscopeRef[] = [
+  {startDate: 21, startMonth: 3, endDate: 19, endMonth: 4, name: "Aries (Ram)"}, 
+  {startDate: 20, startMonth: 4, endDate: 20, endMonth: 5, name: "Taurus (Bull)"}, 
+  {startDate: 21, startMonth: 5, endDate: 20, endMonth: 6, name: "Gemini (Twins)"}, 
+  {startDate: 22, startMonth: 6, endDate: 22, endMonth: 7, name: "Cancer (Crab)"}, 
+  {startDate: 23, startMonth: 7, endDate: 22, endMonth: 8, name: "Leon (Lion)"}, 
+  {startDate: 23, startMonth: 8, endDate: 22, endMonth: 9, name: "Virgo (Virgin)"}, 
+  {startDate: 23, startMonth: 9, endDate: 23, endMonth: 10, name: "Libra (Balance)"}, 
+  {startDate: 24, startMonth: 10, endDate: 21, endMonth: 11, name: "Scorpio (Scorpion)"}, 
+  {startDate: 22, startMonth: 11, endDate: 21, endMonth: 12, name: "Sagittarius (Archer)"}, 
+  {startDate: 22, startMonth: 12, endDate: 19, endMonth: 1, name: "Carpicornnus (Goat)"}, 
+  {startDate: 20, startMonth: 1, endDate: 18, endMonth: 2, name: "Aquaris (Water Bearer)"}, 
+  {startDate: 19, startMonth: 2, endDate: 20, endMonth: 3, name: "Pisces (Fish)"}, 
+];
+
+// YYYY-mm-dd
+function calculateHoroscope(birthday: string): HoroscopeRef {
+  const parts = birthday.split("-");
+  const date = parseInt(parts[2]);
+  const month = parseInt(parts[1]);
+
+  let i: number;
+  for (i=0; i<horoscopeReferences.length; i++) {
+    if (horoscopeReferences[i].startMonth === month) {
+      if (date >= horoscopeReferences[i].startDate) {
+        return horoscopeReferences[i];
+      } else {
+        return i === 0 ? horoscopeReferences[horoscopeReferences.length - 1] : horoscopeReferences[i-1];
+      }
+    }
+  }
+} 
 
 @Injectable()
 export class ProfileService {
